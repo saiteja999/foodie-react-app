97e3aef917e0574097201c3999ca4046ba33aaca
info2214 <info@cric-indo.com>
2024-04-07 16:49:54 -0400

    build: set up Parcel as the module bundler

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
-const heading = React.createElement("h1",{id:"child"},"Hello World from React");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1",{id:"child"},"Hello World 123 from React");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
