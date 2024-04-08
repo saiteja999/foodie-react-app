47cf80bd3026ddf897f5ba70941a0bd7d340cc83
info2214 <info@cric-indo.com>
2024-04-07 23:15:31 -0400

    feat: explore nested component and element patterns

--- App.js
+++ App.js
@@ -5,26 +5,30 @@ const Title = () => (
   <h1 className="title" tabIndex="5">
     Title Component - invoked in HeadingComponent
   </h1>
-)
+);
 
 const number = 1000;
 
+const reactElement = (<h1>React Element</h1>);
 //Component Composition
 const HeadingComponent = () => (
   <div>
+    {number}
+    {reactElement}
     <Title />
     <h1>Heading Component</h1>
   </div>
 );
 
+
 //React element can be placed in {}
 const jsxHeading = (
-    <h1 className="heading" tabIndex="5">
-    <HeadingComponent/>
-      Hello React
-    </h1>
-  );
-
+  <h1 className="heading" tabIndex="5">
+    {reactElement}
+    <HeadingComponent />
+    Hello React
+  </h1>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(jsxHeading);
