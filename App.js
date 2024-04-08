79f7d9cea763a46ef50813e51afce38a81ef95ab
info2214 <info@cric-indo.com>
2024-04-07 22:30:14 -0400

    refactor: convert arrow functions to named function declarations

--- App.js
+++ App.js
@@ -1,22 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => (
-  <h1 className="title" tabIndex="5">
-    Title Component - invoked in HeadingComponent
-  </h1>
-);
+const Title = function () {
+  return (
+    <h1 className="title" tabIndex="5">
+      Title Component - invoked in HeadingComponent
+    </h1>
+  );
+};
 
 //Component Composition
-const HeadingComponent = () => (
-  <div>
-    <Title />
-    <h1>Heading Component</h1>
-  </div>
-);
-
-//const HeadingComponent = () => <h1>Functional Component</h1>
+const HeadingComponent = function () {
+  return (
+    <div>
+      <Title />
+      <h1>Heading Component</h1>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//root.render(HeadingComponent());
 root.render(<HeadingComponent />);
