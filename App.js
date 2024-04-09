72f483e43ebe4aa3bdfe85100f4f30eaa5c41188
info2214 <info@cric-indo.com>
2024-04-08 22:26:30 -0400

    refactor: destructure props in RestaurantCard

--- App.js
+++ App.js
@@ -22,12 +22,14 @@ const Header = () => {
 
 
 const RestaurantCard = (props) =>{
-  console.log(props)
+  
+  const {restaurant, cuisine} = props;
+
   return(
     <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
       <img className="res-logo" src = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/irnwr59vlrmciau11or0" alt ="" />
-      <h3>{props.resName}</h3>
-      <h4>{props.cuisine}</h4>
+      <h3>{restaurant}</h3>
+      <h4>{cuisine}</h4>
       <h4> 4.5 stars </h4>
       <h4> 38 mins </h4>
     </div>
