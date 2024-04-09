ea7c6ddeefe274387154ed178c82022b858a6468
info2214 <info@cric-indo.com>
2024-04-08 22:24:49 -0400

    feat: pass restaurant data to cards via props

--- App.js
+++ App.js
@@ -21,12 +21,13 @@ const Header = () => {
 };
 
 
-const RestaurantCard = () =>{
+const RestaurantCard = (props) =>{
+  console.log(props)
   return(
     <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
       <img className="res-logo" src = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/irnwr59vlrmciau11or0" alt ="" />
-      <h3>Meghana Foods</h3>
-      <h4>Vijayawada Gangur Biryani</h4>
+      <h3>{props.resName}</h3>
+      <h4>{props.cuisine}</h4>
       <h4> 4.5 stars </h4>
       <h4> 38 mins </h4>
     </div>
@@ -38,11 +39,8 @@ const Body = () => {
   <div className="Body">
     <div className="search"> search </div>
     <div className="res-container">
-      <RestaurantCard/>
-      <RestaurantCard/>
-      <RestaurantCard/>
-      <RestaurantCard/>
-      <RestaurantCard/>
+      <RestaurantCard resName = "Meghana Foods" cuisine = "Biryani, Fast Food"/>
+      <RestaurantCard resName = "KFC" cuisine = "French Fries"/>
     </div>
   </div>);
 };
