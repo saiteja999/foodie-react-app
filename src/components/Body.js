3e8c29c832cdf5586022d0263b3821d7cda71b52
info2214 <info@cric-indo.com>
2024-04-13 23:51:05 -0400

    feat: fetch live restaurant data from Swiggy API

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,6 +5,25 @@ import resList from "../utils/mockData";
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState(resList);
 
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    console.log(
+      json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+    );
+
+    setListOfRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    console.log(listOfRestaurants);
+  };
+
+  useEffect(fetchData, []);
+
   return (
     <div className="Body">
       <div className="filter">
@@ -14,6 +33,7 @@ const Body = () => {
             const filteredList = listOfRestaurants.filter((res) => {
               return res.data.avgRating > 4;
             });
+            console.log(filteredList);
             setListOfRestaurants(filteredList);
           }}
         >
